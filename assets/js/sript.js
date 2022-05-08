eb6b6a5afa35e96ac9e77d676dcea96068d6dfb4
Joey Byrd <joeybyrd9999@gmail.com>
2022-05-07 22:47:24 -0700

    created html via java for list

--- assets/js/sript.js
+++ assets/js/sript.js
@@ -4,10 +4,22 @@ var tasksToDoE1 = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) {
 
     event.preventDefault();
+
+    var taskNameInput = document.querySelector("input[name='task-name'").value;
     
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //create list item
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
+    //create div to hold task info and add to list item
+    taskInfoEl.className = "task-info";
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>"
+    
+    listItemE1.textContent = (taskInfoEl);
+
+    // add entire list item to list
     tasksToDoE1.appendChild(listItemE1);
     };
  
